8e9813f3e9402222584173c6ad226a2dabcdc5c2
Ronald Ibañez <ronald_reir21@hotmail.com>
2018-06-21 17:11:40 -0600

    No dio conflicto agregar el sqlserver

---
+++ BackendMantenimiento/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BackendMantenimiento.Models;
+
+namespace BackendMantenimiento.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+        {
+        }
+        public DbSet<Marca> Marcas{ get; set;  }
+    }
+}
---
+++ BackendMantenimiento/Models/Marca.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendMantenimiento.Models
+{
+    public class Marca
+    {
+
+        public long IdMarca { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String Descripcion { get; set; }
+
+        public String Telefono { get; set; }
+
+        public String Email { get; set; }
+
+        public String Direccion { get; set; }
+
+        public String Website { get; set; }
+
+        public String Fundador { get; set; }
+    }
+}
